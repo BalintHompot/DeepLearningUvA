f5f64e6f5cb268369b4d58ccfdc0631ec9d11e4e
BalintHompot <hompot.balint@gmail.com>
2019-11-15 13:17:18 +0100

    dcs

--- Assignment 1/code/train_convnet_pytorch.py
+++ Assignment 1/code/train_convnet_pytorch.py
@@ -71,11 +71,13 @@ def train():
   ## transforming one-hot labels to class labels for loss function
   test_labels_class = np.argmax(test_labels, 1)
 
+  
   X_test, Y_test = Variable(torch.Tensor(test_data)), Variable(torch.Tensor(test_labels_class))
   if torch.cuda.is_available():
     X_test = X_test.cuda()
     Y_test = Y_test.cuda()  
   
+  print("test set moved to gpu")
   training_accuracies = []
   test_accuracies = []
   training_losses = []
@@ -139,11 +141,6 @@ def train():
     epoch_acc += acc
     epoch_loss += loss
     batchCounter += 1
-
-    ## freeing
-    if torch.cuda.is_available():
-      del X
-      del Y
 
   drawPlot(training_accuracies, test_accuracies, './cnn-accuracies.png', 'ConvNet - accuracies on training and test data', 3)
   drawPlot(training_losses, test_losses, './cnn-loss_numpy.png', 'ConvNet - loss on training and test data', 4)
